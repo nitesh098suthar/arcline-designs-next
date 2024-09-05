6efd022103aed1c6ecafa84c9ccefce802e7353a
Nitesh Kumar <nitesh098suthar@gmail.com>
2024-09-05 16:13:19 +0530

    get all design route created

--- app/api/v1/design/route.ts
+++ app/api/v1/design/route.ts
@@ -1,4 +1,17 @@
 import { NextResponse } from "next/server";
+import connectDB from "@/lib/database.js";
+import Design from "@/Model/Design.js";
+
+//get all designs
+export async function GET() {
+  await connectDB();
+  const allListings = await Design.find();
+  
+  return NextResponse.json({
+    success: true,
+    allListings,
+  });
+}
 
 export async function POST() {
   return NextResponse.json({
